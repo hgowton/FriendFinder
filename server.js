13d8148090ac1983f3591b25aedc404c2db1044f
hgowton <hgowton@gmail.com>
2020-03-21 08:36:31 -0400

    Framework for html pages established.  Server.js linked to both html pages through express.

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path')
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Routes
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, '/app/public/home.html'));
+})
+
+app.get('/survey', function(req,res){
+    res.sendFile(path.join(__dirname, '/app/public/survey.html'));
+})
+
+// Your server.js file should require the basic npm packages we've used in class: express and path.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
